3203403d2d2fc6753494b70f99a191533b1f0a64
Ladislav Prskavec <ladislav@prskavec.net>
2016-02-19 14:40:45 +0100

    Better naming

--- test/index_test.js
+++ test/index_test.js
@@ -4,14 +4,14 @@ var headers = require('../lib/headers');
 describe('Test headers', function(){
   it ('Check normal header', function(){
     var headersObject = {'Content-Type':'application/json'};
-    assert.equal(headers.checkHeaders(headersObject), false);
+    assert.equal(headers.isValid(headersObject), true);
   });
   it ('Check invalid header', function(){
     var headersObject = {'Příliš':'žluťoučký kůň'};
-    assert.equal(headers.checkHeaders(headersObject), true);
+    assert.equal(headers.isValid(headersObject), false);
   });
   it ('Check invalid header', function(){
     var headersObject = {'Content-Type':'žluťoučký kůň'};
-    assert.equal(headers.checkHeaders(headersObject), true);
+    assert.equal(headers.isValid(headersObject), false);
   });
 });
